3b4dd9d0ae96f788baaa2de1af56ccb18d050e4a
Taras Potochilov <t.potochilov@bintime.com>
2023-11-04 15:32:50 +0530

    2619 resolved

---
+++ easy/2619.js
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/array-prototype-last/
+
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    // "this" will be array and need to find last element of it or return -1
+    return this.length ? this[this.length - 1] : -1;
+};
+
+const arr = [1, 2, 3];
+arr.last(); // 3
