04cba2490540a8d7e8fb8abab83e6bed7c81ad17
Taras Potochilov <t.potochilov@bintime.com>
2023-11-27 09:55:12 +0530

    2621 resolved

---
+++ easy/2621-sleep.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    return new Promise((resolve) => { setTimeout(resolve, millis) });
+}
+
+const t = Date.now();
+sleep(100).then(() => {
+    console.log(Date.now() - t); // about 100
+});
